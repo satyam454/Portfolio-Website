3a64229f6b7d0e5cf76c47a68dee07224956ce09
satyam454 <130743326+satyam454@users.noreply.github.com>
2023-10-18 12:16:05 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Satyam Shrivastava</span> To acquire a position in a fast-paced web agency and become one of the experts in Web Development. Wishful to utilize my groundbreaking creative skills resulting in my and the company's growth. Core skills: Reactjs, Javascript, CSS, HTML5. "
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
